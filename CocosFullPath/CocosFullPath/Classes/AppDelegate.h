fc3083785ca25903753a41d62d7ea7a2e4ce49d1
ITIW <itiwnewmac2@Shaon-mac2.local>
2014-06-21 18:12:51 +0600

    Full path of main bundle

---
+++ CocosFullPath/CocosFullPath/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  CocosFullPath
+//
+//  Created by itiw-mac on 6/19/14.
+//  Copyright itiw-mac 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
---
+++ CocosFullPath/CocosFullPath/Supporting Files/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CocosFullPath
+//
+//  Created by itiw-mac on 6/19/14.
+//  Copyright itiw-mac 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+        return retVal;
+    }
+}
